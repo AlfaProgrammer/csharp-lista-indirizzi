860c1bbd89107191537d6212e0368156ccb8d539
AlfaProgrammer <alexloghin@outlook.it>
2022-07-05 16:13:28 +0200

    Creato classe Address e Lista Indirizzi ... Sto continuando a fare varie prove

---
+++ Address.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int ZIP { get; set; }
+
+        public static readonly List<Address> _addresses = new List<Address>();
+
+        public void PrintAdresses()
+        {
+            foreach (var address in Address._addresses)
+            {
+                Console.WriteLine("##### Indirizzo #####");
+                Console.WriteLine(address);
+                Console.WriteLine("#", 15);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ListaIndirizzi;
+
 StreamReader address = File.OpenText(@"C:\Users\alexl\source\repos\ListaIndirizzi\addresses.csv");
 
 string format = address.ReadLine();
@@ -9,6 +11,19 @@ string format = address.ReadLine();
     {
         string row = address.ReadLine();
         Console.WriteLine(row);
+
+        string[] data = row.Split(",");       
+
+
+        Address._addresses.Add(new Address()
+        {
+            Name = data[0],
+            Surname = data[1],
+            Street = data[2],
+            City = data[3],
+            Province = data[4],
+            ZIP = int.Parse(data[5]),
+        });
     }
 
 address.Close();
